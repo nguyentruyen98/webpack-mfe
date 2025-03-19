45760ae73126f6fe99f119a67899e16782fee030
nguyentruyen98 <haohaothieugia@gmail.com>
2025-03-20 01:17:25 +0700

    initialize microfrontend architecture with container and marketing apps

---
+++ packages/container/src/components/MarketingApp.js
@@ -0,0 +1,16 @@
+import { mount } from "marketing/Marketing";
+import React, { useRef, useEffect } from "react";
+
+const MarketingApp = () => {
+  const ref = useRef(null);
+  useEffect(() => {
+    mount(ref.current);
+  }, []);
+  return (
+    <div className="test" ref={ref}>
+      MarketingApp
+    </div>
+  );
+};
+
+export default MarketingApp;
