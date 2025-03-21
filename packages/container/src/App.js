486ac88d24f142b635187fe1f615b026c9bcff85
truyen-infotrack-vn <truyen.nguyen@infotrack.com.au>
2025-03-21 13:48:31 +0700

    chore: Update App.js to use Material-UI StylesProvider for styling

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,16 +1,22 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import AppHeader from "./components/Header";
-
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from "@material-ui/core/styles";
 import MarketingApp from "./components/MarketingApp";
 const App = () => {
+  const generateClassName = createGenerateClassName({ productionPrefix: "co" });
   return (
-    <div>
-      <BrowserRouter>
-        <AppHeader />
-        <MarketingApp />
-      </BrowserRouter>
-    </div>
+    <BrowserRouter>
+      <StylesProvider generateClassName={generateClassName}>
+        <div>
+          <AppHeader />
+          <MarketingApp />
+        </div>
+      </StylesProvider>
+    </BrowserRouter>
   );
 };
 
