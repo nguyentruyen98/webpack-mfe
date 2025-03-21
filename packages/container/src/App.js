9630df9e7f26c1499de9d702df1b4541d18f03f8
truyen-infotrack-vn <truyen.nguyen@infotrack.com.au>
2025-03-21 11:01:58 +0700

    chore: Add header component to App.js and update routing configuration

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,7 +1,17 @@
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import AppHeader from "./components/Header";
+
 import MarketingApp from "./components/MarketingApp";
 const App = () => {
-  return <MarketingApp />;
+  return (
+    <div>
+      <BrowserRouter>
+        <AppHeader />
+        <MarketingApp />
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
